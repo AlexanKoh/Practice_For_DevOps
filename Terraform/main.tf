761132dfb99ae3ef0877315de4ed04de362c2959
Alexander <133887502+AlexanKoh@users.noreply.github.com>
2025-06-01 19:41:43 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,26 +11,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0953476d60561c955"
-  instance_type = "t2.micro"
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo amazon-linux-extras install docker -y
-              sudo service docker start
-              sudo docker run -d \
-                -p 80:80 \
-                --restart always \
-                your-dockerhub-username/devops-site:latest
-              EOF
-
-  vpc_security_group_ids = [aws_security_group.app.id]
-
-  tags = {
-    Name = "DevOps-Site-Container"
-  }
+data "aws_ssm_parameter" "amazon_linux_2" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
 resource "aws_security_group" "app" {
@@ -48,5 +30,24 @@ resource "aws_security_group" "app" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ssm_parameter.amazon_linux_2.value
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.app.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              service docker start
+              docker run -d -p 80:80 --restart always your-dockerhub-username/devops-site:latest
+              EOF
+
+  tags = {
+    Name = "DevOps-Site-Container"
   }
 }
