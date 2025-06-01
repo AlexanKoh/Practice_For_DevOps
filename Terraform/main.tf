2196a252466724ae2fe89b3a84d8e0c48343aa69
Alexander Kokhanov <kohanov9@gmail.com>
2025-06-01 20:09:03 +0200

    change main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
   instance_type = "t2.micro"
 
   user_data = <<-EOF
