08e8195b463301d1a557a81982fbe11ba54fc71c
Alexander Kokhanov <kohanov9@gmail.com>
2025-11-16 19:53:03 +0100

    added Kubernetes

---
+++ Kubernetes/Terraform/main.tf
@@ -0,0 +1,51 @@
+resource "aws_security_group" "app" {
+  name        = "devops-site-sg"
+  description = "Security group for Kubernetes EC2 node"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.app.id]
+  key_name               = "For_my_site_EC2"
+
+  tags = {
+    Name = "K8s-Node"
+  }
+}
+
